5c75bd0d01be8da37197fe99e813c25d68bab7e3
jacob.johanssonludv <jacob.johanssonludvigsson@elev.ga.ntig.se>
2023-08-31 11:01:19 +0200

    ✨ variable

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
-console.log ("hello worlds")
+console.log ("🤑FREE BUCKS!!! Get a Free Vbucks using our latest Fortnite Vbucks Generator application 🤑🤑")
 
+let firstName; 
+firstName = "Jens"
+
+let LastName = "andersson"
+
+let num = 23
+console.log(typeof(num));
+
+const NAME = "stina"
+
+
+
+
+for (let i = 0; i < 10; i++) {
+    console.log(NAME)
+}
+
+
+let a = "Jens"
+let b = `jens`  
+
+let c = `mitt namn är $(a)`
+
+let d = `mmitt namn är `+ b
+
+let e = 12
+let r = 16.7
+
+let g = true
+let h = false
+
+console.log(c, e, g)
